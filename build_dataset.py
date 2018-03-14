2764107c4c97d33c57d5c93cfe8a50cdd1e6da10
Chandler <watson@facni.com>
2018-03-14 01:16:49 -0700

    Write core data preprocessing

---
+++ build_dataset.py
@@ -0,0 +1,63 @@
+"""Preprocesses the KITTI dataset and split into train/val/test."""
+
+import argparse
+import glob
+import os
+import random
+import shutil
+from tqdm import tqdm
+
+SPLIT = 0.8
+SEED = 230
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir', default='data/KITTI', help="Directory with the KITTI dataset")
+parser.add_argument('--output_dir', default='data/preproc_KITTI', help="Where to write the new data")
+
+def data_file_to_gt(data_file):
+    parts = data_file.split('_')
+    parts.insert(1, 'road')
+    return '_'.join(parts)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    train_data_dir = os.path.join(args.data_dir, 'training', 'image_2')
+    train_gt_data_dir = os.path.join(args.data_dir, 'training', 'gt_image_2')
+    test_data_dir = os.path.join(args.data_dir, 'testing', 'image_2')
+
+    train_val_files = os.listdir(train_data_dir)
+    train_val_gt_files= os.listdir(train_gt_data_dir)
+    test_files = os.listdir(test_data_dir)
+
+    random.seed(SEED)
+    train_val_files.sort()
+    random.shuffle(train_val_files)
+
+    split_index = int(SPLIT * len(train_val_files))
+    train_files, val_files = train_val_files[:split_index], train_val_files[split_index:]
+
+    train_gt_files = [data_file_to_gt(train_file) for train_file in train_files]
+    val_gt_files = [data_file_to_gt(val_file) for val_file in val_files]
+
+    os.mkdir(args.output_dir)
+
+    train_output_dir = os.path.join(args.output_dir, 'train')
+    val_output_dir = os.path.join(args.output_dir, 'val')
+    test_output_dir = os.path.join(args.output_dir, 'test')
+
+    for output_dir in [train_output_dir, val_output_dir, test_output_dir]:
+        os.mkdir(output_dir)
+
+    data_map = [(train_data_dir, train_files, train_output_dir),
+                (train_gt_data_dir, train_gt_files, train_output_dir),
+                (train_data_dir, val_files, val_output_dir),
+                (train_gt_data_dir, val_gt_files, val_output_dir),
+                (test_data_dir, test_files, test_output_dir)]
+
+    for data_dir, data_files, output_dir in data_map:
+        for data_file in tqdm(data_files):
+            from_path = os.path.join(data_dir, data_file)
+            shutil.copy(from_path, output_dir)
+
+
---
+++ model/model.py
@@ -0,0 +1,78 @@
+"""Define the model."""
+
+import tensorflow as tf
+
+class FCNVGG16:
+    def __init__(self):
+        self.conv1_1 = self.conv_layer(bgr, "conv1_1")
+        self.conv1_2 = self.conv_layer(self.conv1_1, "conv1_2")
+        self.pool1 = self.max_pool(self.conv1_2, 'pool1')
+
+        self.conv2_1 = self.conv_layer(self.pool1, "conv2_1")
+        self.conv2_2 = self.conv_layer(self.conv2_1, "conv2_2")
+        self.pool2 = self.max_pool(self.conv2_2, 'pool2')
+
+        self.conv3_1 = self.conv_layer(self.pool2, "conv3_1")
+        self.conv3_2 = self.conv_layer(self.conv3_1, "conv3_2")
+        self.conv3_3 = self.conv_layer(self.conv3_2, "conv3_3")
+        self.pool3 = self.max_pool(self.conv3_3, 'pool3')
+
+        self.conv4_1 = self.conv_layer(self.pool3, "conv4_1")
+        self.conv4_2 = self.conv_layer(self.conv4_1, "conv4_2")
+        self.conv4_3 = self.conv_layer(self.conv4_2, "conv4_3")
+        self.pool4 = self.max_pool(self.conv4_3, 'pool4')
+
+        self.conv5_1 = self.conv_layer(self.pool4, "conv5_1")
+        self.conv5_2 = self.conv_layer(self.conv5_1, "conv5_2")
+        self.conv5_3 = self.conv_layer(self.conv5_2, "conv5_3")
+        self.pool5 = self.max_pool(self.conv5_3, 'pool5')
+
+        self.fc6 = self.conv_fc_layer(self.pool5, "fc6")
+        self.relu6 = tf.nn.relu(self.fc6)
+
+        self.fc7 = self.conv_fc_layer(self.relu6, "fc7")
+        self.relu7 = tf.nn.relu(self.fc7)
+
+        self.fc8 = self.conv_fc_layer(self.relu7, "fc8")
+
+        self.prob = tf.nn.softmax(self.fc8, name="prob")
+
+
+    def conv_layer(self, bottom, name):
+        with tf.variable_scope(name) as scope:
+            filt = self.get_conv_filter(name)
+            conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding='SAME')
+            conv_biases = self.get_bias(name)
+            bias = tf.nn.bias_add(conv, conv_biases)
+            relu = tf.nn.relu(bias)
+        return relu
+
+
+    def conv_fc_layer(self, bottom, name):
+        with tf.variable_scope(name) as scope:
+            shape = bottom.get_shape().as_list()
+
+            if name == 'fc6':
+                filt = self.get_fc_weight_reshape(name, [7, 7, 512, 4096])
+            elif name == 'score_fr':
+                name = 'fc8'  # Name of score_fr layer in VGG Model
+                filt = self.get_fc_weight_reshape(name, [1, 1, 4096, 1000],
+                                                  num_classes=num_classes)
+            else:
+                filt = self.get_fc_weight_reshape(name, [1, 1, 4096, 4096])
+            conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding='SAME')
+            conv_biases = self.get_bias(name, num_classes=num_classes)
+            bias = tf.nn.bias_add(conv, conv_biases)
+
+            if relu:
+                bias = tf.nn.relu(bias)
+            _activation_summary(bias)
+
+            if debug:
+                bias = tf.Print(bias, [tf.shape(bias)],
+                                message='Shape of %s' % name,
+                                summarize=4, first_n=1)
+            return bias
+
+
+
